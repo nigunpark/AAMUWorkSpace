bccf5387ab87ac0e0270ec938784d1f1fe332820
kim ki moon <98200337+MoZZANG@users.noreply.github.com>
2022-06-29 18:16:15 +0900

    Update MainPage.js

--- AAMUFrountApp/src/pages/MainPage/MainPage.js
+++ AAMUFrountApp/src/pages/MainPage/MainPage.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./MainPage.css";
 import KMap from "../../components/KMap/KMap.js";
 import { useParams } from "react-router-dom";
 import LeftPlanSide from "../../components/LeftPlanSide/LeftPlanSide";
 import RightRecommandSide from "../../components/RightRecommandSide/RightRecommandSide";
+import CreatePlan from "../../components/CreatePlan/CreatePlan";
 const MainPage = () => {
   const [titleName, setTitleName] = useState("");
   const [conWhichModal, setConWhichModal] = useState(false);
   let { currPosition } = useParams();
+  const [showCreatePlan, setShowCratePlan] = useState(false);
+  useEffect(() => {
+    console.log("mainpage useEffect");
+  }, [CreatePlan]);
   return (
     <div className="MainPage">
       <LeftPlanSide currPosition={currPosition} />
@@ -15,12 +20,20 @@ const MainPage = () => {
         currPosition={currPosition}
         setTitleName={setTitleName}
         setConWhichModal={setConWhichModal}
+        setShowCratePlan={setShowCratePlan}
       />
+
       <RightRecommandSide
         setTitleName={setTitleName}
         titleName={titleName}
         conWhichModal={conWhichModal}
       />
+      {showCreatePlan ? (
+        <CreatePlan
+          setShowCratePlan={setShowCratePlan}
+          showCreatePlan={showCreatePlan}
+        />
+      ) : null}
     </div>
   );
 };
