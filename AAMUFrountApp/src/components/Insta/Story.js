3972215c532ee86a78eaef8f0bd2d5efa996119d
jiyellie <106718961+jiyellie@users.noreply.github.com>
2022-06-29 18:52:37 +0900

    Create Story.js

---
+++ AAMUFrountApp/src/components/Insta/Story.js
@@ -0,0 +1,76 @@
+import React from 'react'
+
+function Story() {
+  return (
+    <div className="story">
+            <div className="story-down">
+                <div className="story-contents">
+                    <div className="gradient">
+                        <img src="img/jenny.jpeg" alt="스프" />
+                    </div>
+                    <div>
+                        <p className="user-id">jenny0305</p>
+                    </div>
+                </div>
+                <div className="story-contents">
+                    <div className="gradient">
+                        <img src="img/woziin.jpeg" alt="스프" />
+                    </div>
+                    <div>
+                        <p className="user-id">wooziin</p>
+                    </div>
+                </div>
+                <div className="story-contents">
+                    <div className="gradient">
+                        <img src="img/jenny.jpeg" alt="스프" />
+                    </div>
+                    <div>
+                        <p className="user-id">jenny0305</p>
+                    </div>
+                </div>
+                <div className="story-contents">
+                    <div className="gradient">
+                        <img src="img/woziin.jpeg" alt="스프" />
+                    </div>
+                    <div>
+                        <p className="user-id">jiye0101</p>
+                    </div>
+                </div>
+                <div className="story-contents">
+                    <div className="gradient">
+                        <img src="img/go.jpeg" alt="스프" />
+                    </div>                            
+                    <div>
+                        <p className="user-id">jiiii11</p>
+                    </div>
+                </div>
+                <div className="story-contents">
+                    <div className="gradient">
+                        <img src="img/local.jpeg" alt="스프" />
+                    </div>                            
+                    <div>
+                        <p className="user-id">lockalkim</p>
+                    </div>
+                </div>
+                <div className="story-contents">
+                    <div className="gradient">
+                        <img src="img/177919409_466024897787486_7015720230700328365_n.jpeg" alt="스프" />
+                    </div>                            
+                    <div>
+                        <p className="user-id">psg</p>
+                    </div>
+                </div>
+                <div className="story-contents">
+                    <div className="gradient">
+                        <img src="img/177919409_466024897787486_7015720230700328365_n.jpeg" alt="스프" />
+                    </div>                            
+                    <div>
+                        <p className="user-id">0hyun0hyun</p>
+                    </div>
+                </div>
+            </div>    
+        </div>
+  )
+}
+
+export default Story
