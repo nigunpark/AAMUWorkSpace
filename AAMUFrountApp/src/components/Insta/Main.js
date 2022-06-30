449fbb3fda6e08047e90bf8aeedcdba296ef66b9
kim ki moon <98200337+MoZZANG@users.noreply.github.com>
2022-06-30 18:05:52 +0900

    Update Main.js

--- AAMUFrountApp/src/components/Insta/Main.js
+++ AAMUFrountApp/src/components/Insta/Main.js
@@ -1,33 +1,27 @@
-import React, { Component } from 'react';
-import './Main.scss';
-import { Routes, Route, Link } from 'react-router-dom';
-import Nav from './Nav.js';
-import FeedSetting from './FeedSetting.js';
-import User from './User.js';
-import Story from './Story.js';
-import Imgup from './Imgup.js'
+import React, { Component } from "react";
+import "./Main.scss";
+import { Routes, Route, Link } from "react-router-dom";
+import Nav from "./Nav.js";
+import FeedSetting from "./FeedSetting.js";
+import User from "./User.js";
+import Story from "./Story.js";
+import Imgup from "./Imgup.js";
 
 function Main() {
-    return (
-      <div className="main">
-        <Nav></Nav>
-          {/* <div className="margin-top"> 
+  return (
+    <div className="main">
+      <Nav></Nav>
+      {/* <div className="margin-top"> 
             <Story></Story>   
           </div> */}
-          <div className="margin-value"> 
-            <FeedSetting></FeedSetting>
-            <div className="main-right">   
-                <User></User>                   
-            </div>
-          </div>
+      <div className="margin-value">
+        <FeedSetting />
+        <div className="main-right">
+          <User />
+        </div>
       </div>
-    );    
+    </div>
+  );
 }
 
 export default Main;
-
-
-
-
-
-
