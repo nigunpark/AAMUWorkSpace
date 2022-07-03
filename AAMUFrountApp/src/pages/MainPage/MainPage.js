70e0620afd8fb2a7332d00fb8528be7a7620cedf
MoZZANG <kkm0938@naver.com>
2022-07-03 19:50:07 +0900

    Update MainPage.js

--- AAMUFrountApp/src/pages/MainPage/MainPage.js
+++ AAMUFrountApp/src/pages/MainPage/MainPage.js
@@ -5,18 +5,23 @@ import { useParams } from "react-router-dom";
 import LeftPlanSide from "../../components/LeftPlanSide/LeftPlanSide";
 import RightRecommandSide from "../../components/RightRecommandSide/RightRecommandSide";
 import CreatePlan from "../../components/CreatePlan/CreatePlan";
-import { useSelector, useDispatch } from "react-redux";
-import { delAllSaveDaysRedux, deleteAllPickJanso } from "../../redux/store";
+import { useDispatch } from "react-redux";
+import {
+  delAllSaveDaysRedux,
+  deleteAllPickJanso,
+  clearLatLng,
+  setLatLng,
+} from "../../redux/store";
+const { kakao } = window;
 const MainPage = () => {
   const [titleName, setTitleName] = useState("");
   const [conWhichModal, setConWhichModal] = useState(false);
   let { currPosition } = useParams();
   const [showCreatePlan, setShowCratePlan] = useState(false);
-  let reduxState = useSelector((state) => {
-    return state;
-  });
+  const [stateForMapCenter, setStateForMapCenter] = useState([]);
   let dispatch = useDispatch();
   useEffect(() => {
+    getLatLng(currPosition, setStateForMapCenter, stateForMapCenter);
     dispatch(deleteAllPickJanso([]));
     dispatch(delAllSaveDaysRedux([]));
   }, []);
@@ -28,6 +33,7 @@ const MainPage = () => {
         setTitleName={setTitleName}
         setConWhichModal={setConWhichModal}
         setShowCratePlan={setShowCratePlan}
+        stateForMapCenter={stateForMapCenter}
       />
 
       <RightRecommandSide
@@ -39,10 +45,24 @@ const MainPage = () => {
         <CreatePlan
           setShowCratePlan={setShowCratePlan}
           showCreatePlan={showCreatePlan}
+          currPosition={currPosition}
         />
       ) : null}
     </div>
   );
 };
-
+const getLatLng = (currPosition, setStateForMapCenter, stateForMapCenter) => {
+  var geocoder = new kakao.maps.services.Geocoder();
+  // 주소로 좌표를 검색합니다
+  geocoder.addressSearch(currPosition, (result, status) => {
+    // 정상적으로 검색이 완료됐으면
+    if (status === kakao.maps.services.Status.OK) {
+      // var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+      let temp = [...stateForMapCenter];
+      temp.push(result[0].y);
+      temp.push(result[0].x);
+      setStateForMapCenter(temp);
+    }
+  });
+};
 export default MainPage;
