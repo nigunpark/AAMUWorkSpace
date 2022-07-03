fcc2daea126408e363d9c7481b5a8eca3feae6c9
MoZZANG <kkm0938@naver.com>
2022-07-03 19:49:55 +0900

    Update CreatePlanMap.js

--- AAMUFrountApp/src/components/CreatePlan/CreatePlanMap.js
+++ AAMUFrountApp/src/components/CreatePlan/CreatePlanMap.js
@@ -1,29 +1,51 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 const { kakao } = window;
-const CreatePlanMap = ({ showCreatePlan }) => {
+const CreatePlanMap = ({ showCreatePlan, currPosition }) => {
+  let [lat, setLat] = useState("");
+  let [lng, setLng] = useState("");
+  let reduxState = useSelector((state) => {
+    return state;
+  });
   useEffect(() => {
-    var cMapContainer = document.getElementById("cMap"), // 지도를 표시할 div
-      cMapOption = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3, // 지도의 확대 레벨
+    var mapContainer = document.getElementById("cMap"), // 지도를 표시할 div
+      mapOption = {
+        center: new kakao.maps.LatLng(lat, lng), // 지도의 중심좌표
+        level: 9, // 지도의 확대 레벨
       };
 
-    // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-    var map = new kakao.maps.Map(cMapContainer, cMapOption);
+    var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+    var geocoder = new kakao.maps.services.Geocoder();
+    // 주소로 좌표를 검색합니다
+    geocoder.addressSearch(
+      reduxState.localNameForMarker.addr ?? currPosition,
+      (result, status) => {
+        // 정상적으로 검색이 완료됐으면
+        if (status === kakao.maps.services.Status.OK) {
+          var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+          setLat(result[0].y);
+          setLng(result[0].x);
+          // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+          map.panTo(coords);
+        }
+      }
+    );
     map.relayout();
-  }, [showCreatePlan]);
+  }, [lat, lng, showCreatePlan]);
   return (
     <div
       style={{
         width: "100%",
         display: "inline-block",
-      }}>
+      }}
+    >
       <div
         id="cMap"
         style={{
           width: "100%",
           height: "100%",
-        }}>
+        }}
+      >
         aa
       </div>
     </div>
