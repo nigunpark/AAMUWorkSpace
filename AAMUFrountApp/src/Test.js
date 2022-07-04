43ff1f939d12230193004311ca04512435f2bd76
MoZZANG <kkm0938@naver.com>
2022-07-04 22:47:14 +0900

    Update Test.js

--- AAMUFrountApp/src/Test.js
+++ AAMUFrountApp/src/Test.js
@@ -3,6 +3,8 @@ import axios from "axios"; // 액시오스
 import { useLocation } from "react-router-dom";
 
 const Test = () => {
+  let token=sessionStorage.getItem('token');
+  
   // const initializeNaverLogin = () => {
   //   const naverLogin = new naver.LoginWithNaverId({
   //     clientId: "amnFx3UOA94ulL_rvg9Y",
@@ -37,50 +39,57 @@ const Test = () => {
 
   // const api = "http://192.168.0.19:8080";
 
-  // function testAxios() {
-  //   axios
-  //     .get("http://192.168.0.19:8080/rest/info2?areacode=1&contenttypeid=12")
-  //     .then((response) => {
-  //       console.log(response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     })
-  //     .then(() => {
-  //       console.log("get요청실행됨");
-  //     });
-  // }
+  function testAxios() {
+    axios
+      .get("http://192.168.0.19:8080/rest/info2?areacode=1&contenttypeid=12")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .then(() => {
+        console.log("get요청실행됨");
+      });
+  }
 
-  // function test() {
-  //   axios
-  //     .post("http://192.168.0.19:8080/rest/info2", {
-  //       areacode: "1",
-  //       contenttypeid: "12",
-  //     })
-  //     .then((resp) => {
-  //       console.log("post 성공");
-  //       console.log(resp.data);
-  //     })
-  //     .catch((error) => {
-  //       console.log("error : ", error);
-  //     })
-  //     .then(() => {
-  //       console.log("post요청됨");
-  //     });
-
+ 
+  function test() {
+    axios
+      .get("/aamurest/info/places", {
+        Headers:{
+            Authorization:`Bearer ${token}`
+        },
+        params: {
+          areacode: "1",
+          contenttypeid: "12",
+        },
+      })
+      .then((resp) => {
+        console.log("get성공");
+        console.log(resp.data);
+      })
+      .catch((error) => {
+        console.log("error :%O ", error);
+      })
+      .then(() => {
+        console.log("get요청됨");
+      });
+  }
   return (
     <div>
-      {/*     
       <button
         style={{ background: "green", cursor: "pointer" }}
-        onClick={() => testAxios()}>
+        onClick={() => testAxios()}
+      >
         test
       </button>
       <button
         style={{ background: "yellow", cursor: "pointer" }}
-        onClick={() => test()}>
-        axiosTest
-      </button> */}
+        onClick={() => test()}
+      >
+        우재한테 get요청
+      </button>
     </div>
   );
 };
