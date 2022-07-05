dc5482bcba388a96630e03bde541fc16e88d03a5
nigunpark <psh0944@gmail.com>
2022-07-05 16:32:55 +0900

    Update SpringMvcAnnotation.java

--- AAMURestService/src/main/java/com/aamu/aamurest/init/SpringMvcAnnotation.java
+++ AAMURestService/src/main/java/com/aamu/aamurest/init/SpringMvcAnnotation.java
@@ -1,5 +1,8 @@
 package com.aamu.aamurest.init;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class SpringMvcAnnotation extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -17,6 +20,14 @@ public class SpringMvcAnnotation extends AbstractAnnotationConfigDispatcherServl
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
+	}
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return new Filter[]{filter};
 	}
 
 }
