f8629241c1d057ff408c080abc6591e35e2fe11f
MoZZANG <kkm0938@naver.com>
2022-07-06 10:38:38 +0900

    Update Login.js

--- AAMUFrountApp/src/components/Login/Login.js
+++ AAMUFrountApp/src/components/Login/Login.js
@@ -3,8 +3,6 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
@@ -39,91 +37,93 @@ const Login = () => {
       });
   };
   return (
-    <ThemeProvider theme={theme} sx={{ marginTop: 8 }}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "warning.light" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            로그인
-          </Typography>
+    <div>
+      <ThemeProvider theme={theme} sx={{ marginTop: 8 }}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
           <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
+            sx={{
+              marginTop: 11,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-            <TextField
-              color="warning"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="아이디를 입력하세요"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              color="warning"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="비밀번호를 입력하세요"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            {/* <FormControlLabel
+            <Avatar sx={{ m: 1, bgcolor: "warning.light" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              로그인
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                color="warning"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="아이디를 입력하세요"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                color="warning"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="비밀번호를 입력하세요"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              {/* <FormControlLabel
               control={<Checkbox value="remember" color="warning" />}
               label="아이디 저장"
             /> */}
-            <Button
-              color="warning"
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              로그인
-            </Button>
-            <Button
-              // type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              // onClick={loginPost}
-              href={KAKAO_AUTH_URL}
-            >
-              <img src="/images/kakaoLogin.png" />
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  비밀번호를 잊어버리셨나요?
-                </Link>
+              <Button
+                color="warning"
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                로그인
+              </Button>
+              <Button
+                // type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                // onClick={loginPost}
+                href={KAKAO_AUTH_URL}
+              >
+                <img src="/images/kakaoLogin.png" />
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    비밀번호를 잊어버리셨나요?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"회원가입"}
+                  </Link>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"회원가입"}
-                </Link>
-              </Grid>
-            </Grid>
+            </Box>
           </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
+          <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+      </ThemeProvider>
+    </div>
   );
 };
 
