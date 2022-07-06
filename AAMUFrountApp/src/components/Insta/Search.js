a15f65ab06f458dd5dcc5f742449caffcb762c8a
MoZZANG <kkm0938@naver.com>
2022-07-06 19:13:40 +0900

    Update Search.js

--- AAMUFrountApp/src/components/Insta/Search.js
+++ AAMUFrountApp/src/components/Insta/Search.js
@@ -1,32 +1,25 @@
+import React, { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
 
-import React, { useEffect, useRef, useState } from 'react'
-import styled from 'styled-components'
-
-const search = ({setIsOpen}) => {
-
+const search = ({ setIsOpen }) => {
   return (
-    
     <div className="search-all disappear">
-        <div className="search-engine">
-            <div className="parent">
-            <div className="search-contents">
-                <div className="gradient">
-                    <img src="img/jenny.jpeg" alt="스토리 프로필 사진" />
-                </div>
-                <div>
-                    <p className="user-id">jenny0305</p>
-                    <p className="user-name">hi im jenny💙</p>
-                </div>
+      <div className="search-engine">
+        <div className="parent">
+          <div className="search-contents">
+            <div className="gradient">
+              <img src="img/bk.jpg" alt="스토리 프로필 사진" />
             </div>
+            <div>
+              <p className="user-id">jenny0305</p>
+              <p className="user-name">hi im jenny💙</p>
             </div>
-            <div className='search-square'></div>
+          </div>
         </div>
-        
-    </div>  
-                 
-   
-  )
-}
+        <div className="search-square"></div>
+      </div>
+    </div>
+  );
+};
 
-
-export default search
+export default search;
