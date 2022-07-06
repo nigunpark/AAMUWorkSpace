ee7615bd708bfcb60b1063b7ac680d463301dc23
jiyellie <106718961+jiyellie@users.noreply.github.com>
2022-07-06 21:12:25 +0900

    Update Search.js

--- AAMUFrountApp/src/components/Insta/ModalGroup/Search.js
+++ AAMUFrountApp/src/components/Insta/ModalGroup/Search.js
@@ -1,31 +1,32 @@
-
 import React, { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 
 const search = ({setIsOpen}) => {
+    
 
   return (
     
-<div className="search-all disappear">
-<div className="search-squre"></div>
-    <div className="search-engine">
-        <div className="parent">
-        <div className="search-contents">
-            <div className="gradient">
-                <img src="img/jenny.jpeg" alt="스토리 프로필 사진" />
+    <div className="search-all disappear">
+      <div className="search-squre"></div>
+        <div className="search-engine">
+            <div className="parent">
+            <div className="search-contents">
+                <div className="gradient">
+                    <img src="img/jenny.jpeg" alt="스토리 프로필 사진" />
+                </div>
+                <div>
+                    <p className="user-id">jenny0305</p>
+                    <p className="user-name">hi im jenny💙</p>
+                </div>
             </div>
-            <div>
-                <p className="user-id">jenny0305</p>
-                <p className="user-name">hi im jenny💙</p>
             </div>
-        </div>
         </div>
     </div>
-</div>
                  
    
   )
 }
 
-
+ 
+    
 export default search
