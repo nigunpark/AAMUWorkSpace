cb0117e2f25bba22d1536886b4dad0b08693de48
MoZZANG <kkm0938@naver.com>
2022-07-08 10:31:56 +0900

    Update MyPage.js

--- AAMUFrountApp/src/pages/MyPage/MyPage.js
+++ AAMUFrountApp/src/pages/MyPage/MyPage.js
@@ -1,7 +1,3 @@
-import React from "react";
-import "./MyPage.scss";
-import MyBox from "./MyPageBox/MyBox";
-
 import React, { useEffect, useRef, useState } from "react";
 import "./MyPage.scss";
 import MyHomeBox from "./MyPageBox/MyHomeBox";
@@ -341,31 +337,36 @@ const MyPage = () => {
               </svg>
             </button>
 
-            {/* <button ref={setting} className="app-sidebar-link" onClick={()=>{setClickTab(3)}}>
-            <svg
-              className="link-icon feather feather-settings"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-            > */}
-            {/* className="link-icon feather feather-settings" 
+            <button
+              ref={setting}
+              className="app-sidebar-link"
+              onClick={() => {
+                setClickTab(3);
+              }}
+            >
+              <svg
+                className="link-icon feather feather-settings"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+              >
+                {/* className="link-icon feather feather-settings" 
               xmlns="http://www.w3.org/2000/svg" 
               width="24" height="24" fill="none" 
               stroke="currentColor" strokeLinecap="round" 
               strokeLinejoin="round" strokeWidth="2"
-              viewBox="0 0 24 24"> */}
-            {/* <defs />
-              <circle cx="12" cy="12" r="3" />
-              <path d="M19.4 15a1.65 1.65 0 00.33 1.82l.06.06a2 2 0 010 2.83 2 2 0 01-2.83 0l-.06-.06a1.65 1.65 0 00-1.82-.33 1.65 1.65 0 00-1 1.51V21a2 2 0 01-2 2 2 2 0 01-2-2v-.09A1.65 1.65 0 009 19.4a1.65 1.65 0 00-1.82.33l-.06.06a2 2 0 01-2.83 0 2 2 0 010-2.83l.06-.06a1.65 1.65 0 00.33-1.82 1.65 1.65 0 00-1.51-1H3a2 2 0 01-2-2 2 2 0 012-2h.09A1.65 1.65 0 004.6 9a1.65 1.65 0 00-.33-1.82l-.06-.06a2 2 0 010-2.83 2 2 0 012.83 0l.06.06a1.65 1.65 0 001.82.33H9a1.65 1.65 0 001-1.51V3a2 2 0 012-2 2 2 0 012 2v.09a1.65 1.65 0 001 1.51 1.65 1.65 0 001.82-.33l.06-.06a2 2 0 012.83 0 2 2 0 010 2.83l-.06.06a1.65 1.65 0 00-.33 1.82V9a1.65 1.65 0 001.51 1H21a2 2 0 012 2 2 2 0 01-2 2h-.09a1.65 1.65 0 00-1.51 1z" />
-            </svg>
-          </a>
-          </button> */}
+              viewBox="0 0 24 24">  */}
+                <defs />
+                <circle cx="12" cy="12" r="3" />
+                <path d="M19.4 15a1.65 1.65 0 00.33 1.82l.06.06a2 2 0 010 2.83 2 2 0 01-2.83 0l-.06-.06a1.65 1.65 0 00-1.82-.33 1.65 1.65 0 00-1 1.51V21a2 2 0 01-2 2 2 2 0 01-2-2v-.09A1.65 1.65 0 009 19.4a1.65 1.65 0 00-1.82.33l-.06.06a2 2 0 01-2.83 0 2 2 0 010-2.83l.06-.06a1.65 1.65 0 00.33-1.82 1.65 1.65 0 00-1.51-1H3a2 2 0 01-2-2 2 2 0 012-2h.09A1.65 1.65 0 004.6 9a1.65 1.65 0 00-.33-1.82l-.06-.06a2 2 0 010-2.83 2 2 0 012.83 0l.06.06a1.65 1.65 0 001.82.33H9a1.65 1.65 0 001-1.51V3a2 2 0 012-2 2 2 0 012 2v.09a1.65 1.65 0 001 1.51 1.65 1.65 0 001.82-.33l.06-.06a2 2 0 012.83 0 2 2 0 010 2.83l-.06.06a1.65 1.65 0 00-.33 1.82V9a1.65 1.65 0 001.51 1H21a2 2 0 012 2 2 2 0 01-2 2h-.09a1.65 1.65 0 00-1.51 1z" />
+              </svg>
+            </button>
           </div>
 
           <div className="projects-section">
@@ -438,10 +439,10 @@ const MyPage = () => {
             </div>
 
             <div className="project-boxes jsGridView">
+              {/* <MyBox />
               <MyBox />
               <MyBox />
-              <MyBox />
-              <MyBox />
+              <MyBox /> */}
             </div>
           </div>
 
