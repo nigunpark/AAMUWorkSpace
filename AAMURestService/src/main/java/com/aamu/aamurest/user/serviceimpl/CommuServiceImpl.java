faec9831fb8fe73b4f34a15731b0988fdc35a700
kyeongheekim-93 <kkh1656@naver.com>
2022-07-12 21:38:37 +0900

    commuLike_add likecount

--- AAMURestService/src/main/java/com/aamu/aamurest/user/serviceimpl/CommuServiceImpl.java
+++ AAMURestService/src/main/java/com/aamu/aamurest/user/serviceimpl/CommuServiceImpl.java
@@ -37,7 +37,7 @@ public class CommuServiceImpl implements CommuService<CommuDTO>{
 	//글 목록용_좋아요 여부 뿌려주기
 	@Override
 	public Boolean commuIsLike(Map map) {
-		int isLikeaffected=dao.commuIsLike(map);
+		int isLikeaffected=dao.commuLikeSelect(map);
 		if(isLikeaffected == 1) return true;
 		else return false;
 	}
@@ -160,6 +160,12 @@ public class CommuServiceImpl implements CommuService<CommuDTO>{
 			if(commuLikeDeleteAffected==1 && commuLikeUpdateAffected==1) return 1;
 			else return 0;
 		}
+	}
+	
+	//글 좋아요_selectOne(community테이블의 likecount)
+	@Override
+	public int commuLikecountSelect(Map map) {
+		return dao.commuLikecountSelect(map);
 	}
 
 	
