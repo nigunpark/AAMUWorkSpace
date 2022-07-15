f18fd30521a0a637acc7e44ce1a626af72d2d11c
jiyellie <106718961+jiyellie@users.noreply.github.com>
2022-07-16 04:14:11 +0900

    Update ButtonGroup.js

--- AAMUFrountApp/src/components/Insta/ModalGroup/UploadSecond/ButtonGroup.js
+++ AAMUFrountApp/src/components/Insta/ModalGroup/UploadSecond/ButtonGroup.js
@@ -5,29 +5,15 @@ function Dropdown() {
     
 
     return (
-      <Select name="select" class="select">
-          <option disabled selected>선택</option>
+      <select name="select" className="select">
+          <option defaultValue={'DEFAULT'}>선택</option>
           <option value="title">제목</option>
           <option value="id">아이디</option>
           <option value="tag">태그</option>
-      </Select>
+      </select>
     )
 }
  
-const Select = styled.select`
-    background-color:#fff;
-    padding:5px;
-    margin-top:28px;
-    border-radius:3px;
 
-    .items {
-      position: absolute;
-      background-color: DodgerBlue;
-      top: 100%;
-      left: 0;
-      right: 0;
-      z-index: 99;
-    }
-`
 
 export default Dropdown;
