66e30e94693e14eeb467c602a459284ff572fe41
nigunpark <psh0944@gmail.com>
2022-07-15 21:42:24 +0900

    upload file

--- AAMURestService/src/main/java/com/aamu/aamurest/user/service/CommuDTO.java
+++ AAMURestService/src/main/java/com/aamu/aamurest/user/service/CommuDTO.java
@@ -28,6 +28,7 @@ public class CommuDTO {
 	public CommuCommentDTO commuComment;
 	public List<CommuCommentDTO> commuCommentList;
 	public List<String> photo;
+	public List<byte[]> photobyte;
 	public String contentid;
 	public String title;
 }
