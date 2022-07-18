af2b51c324f0afcb8d57bed37873ed9e8ab482db
MoZZANG <kkm0938@naver.com>
2022-07-18 14:11:33 +0900

    Update KMap.js

--- AAMUFrountApp/src/components/KMap/KMap.js
+++ AAMUFrountApp/src/components/KMap/KMap.js
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { useSelector, useDispatch } from "react-redux/es/exports";
 import "./KMap.css";
 import {
+  changeArrForJangso,
   changeArrForSukso,
   changeInfo,
   changePickedTransport,
@@ -39,6 +40,7 @@ import { ContainerInValid, OverlayInValid } from "../Modal/InVaildModal.js";
 const { kakao } = window;
 let mainMarkers = [];
 let markers = [];
+let token = sessionStorage.getItem("token");
 const KMap = ({
   currPosition,
   setTitleName,
@@ -110,17 +112,19 @@ const KMap = ({
 
   //지도 줌인줌아웃시 지도레벨 setting state
   useEffect(() => {
-    if (kMap === null) return;
+    // if (kMap === null) return;
     //지도레벨이벤트
-    kakao.maps.event.addListener(kMap, "zoom_changed", function () {
-      // 지도의 현재 레벨을 얻어옵니다
-      let level = kMap.getLevel();
-      setMapLevel(level);
-      let center = kMap.getCenter();
-      let newCoords = new kakao.maps.LatLng(center.getLat(), center.getLng());
-      kMap.setCenter(newCoords);
-    });
-  }, [container]);
+    // kakao.maps.event.addListener(kMap, "zoom_changed", function () {
+    //   console.log("11", center.getLat());
+    //   // 지도의 현재 레벨을 얻어옵니다
+    //   let level = kMap.getLevel();
+    //   setMapLevel(level);
+    //   let center = kMap.getCenter();
+    //   let newCoords = new kakao.maps.LatLng(center.getLat(), center.getLng());
+    //   kMap.setCenter(newCoords);
+    //   // zoomAxios()
+    // });
+  }, []);
 
   //주소변경시 해당위치로 맵의 중심옮김 & 인포윈도우생성
   useEffect(() => {
@@ -229,24 +233,50 @@ const KMap = ({
       markers.push(marker);
     }
   }, [
-    reduxState.localNameForMarker,
-    reduxState.arrForPickJangso,
+    // reduxState.localNameForMarker,
+    // reduxState.arrForPickJangso,
     pickedJangso,
   ]);
 
-  function roadview() {
-    console.log("로드뷰");
-    var roadviewContainer = document.getElementById("roadview"); //로드뷰를 표시할 div
-    var roadview = new kakao.maps.Roadview(roadviewContainer); //로드뷰 객체
-    var roadviewClient = new kakao.maps.RoadviewClient(); //좌표로부터 로드뷰 파노ID를 가져올 로드뷰 helper객체
+  useEffect(() => {
+    if (kMap === null) return;
 
-    var position = new kakao.maps.LatLng(33.450701, 126.570667);
-
-    // 특정 위치의 좌표와 가까운 로드뷰의 panoId를 추출하여 로드뷰를 띄운다.
-    roadviewClient.getNearestPanoId(position, 50, function (panoId) {
-      roadview.setPanoId(panoId, position); //panoId와 중심좌표를 통해 로드뷰 실행
+    kakao.maps.event.addListener(kMap, "zoom_changed", function () {
+      // console.log("token", token);
+      console.log("줌안");
+      let center = kMap.getCenter();
+      // 지도의 현재 레벨을 얻어옵니다
+      let level = kMap.getLevel();
+      // setMapLevel(level);
+      let newCoords = new kakao.maps.LatLng(center.getLat(), center.getLng());
+      zoomAxios(center.getLat(), center.getLng());
     });
-  }
+    // if (kMap === null) return;
+  }, [handleWheel]);
+
+  const zoomAxios = (placey, placex) => {
+    axios
+      .get("/aamurest/info/recentplace", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: {
+          placey: placey,
+          placex: placex,
+          distance: "7",
+        },
+      })
+      .then((resp) => {
+        console.log(resp.data);
+        // dispatch(changeArrForJangso(resp.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  function handleWheel() {}
+  window.addEventListener("wheel", handleWheel);
+
   return (
     <div>
       <div
