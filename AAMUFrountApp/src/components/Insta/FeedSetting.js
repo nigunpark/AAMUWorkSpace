3d65de7da44bab6e6d3eaaa28e5a93e3d3d517a7
jiyellie <106718961+jiyellie@users.noreply.github.com>
2022-07-18 22:08:56 +0900

    Update FeedSetting.js

--- AAMUFrountApp/src/components/Insta/FeedSetting.js
+++ AAMUFrountApp/src/components/Insta/FeedSetting.js
@@ -189,24 +189,22 @@ function FeedSetting({val}) {
 }
 
 const Container1 = styled.div`
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    z-index: 1000;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+position: fixed;
+width: 100%;
+height: 100%;
+z-index: 1000;
+top: 0;
+left: 0;
+right: 0;
+bottom: 0;
+display: flex;
+justify-content: center;
+align-items: center;
 `
 const Overlay = styled.div`
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    overflow:hidden;
-    background-color: rgba(0, 0, 0, 0.6);
+position: relative;
+width: 100%;
+height: 100%;
+background-color: rgba(0, 0, 0, 0.6);
 `
 export default FeedSetting
