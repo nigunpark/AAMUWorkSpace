b41b3be41a7859fe917e6ffde63593cab550fa23
nigunpark <107670168+nigunpark@users.noreply.github.com>
2022-07-20 00:30:09 +0900

    Update AAMUPlanHome.kt

--- AAMUAndroidApp/app/src/main/java/com/aamu/aamuandroidapp/components/aamuplan/AAMUPlanHome.kt
+++ AAMUAndroidApp/app/src/main/java/com/aamu/aamuandroidapp/components/aamuplan/AAMUPlanHome.kt
@@ -2,6 +2,10 @@ package com.aamu.aamuandroidapp.components.aamuplan
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -23,19 +27,22 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.aamu.aamuandroidapp.ui.theme.amber200
+import com.aamu.aamuandroidapp.ui.theme.orange700
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun AAMUPlanHome(){
     val bottomSheetScaffoldState = rememberBottomSheetScaffoldState()
+    val lazyListState = rememberLazyListState()
     val coroutineScope = rememberCoroutineScope()
-    val mapviewModel : AAMUPlanViewModel = viewModel(
-        factory = AAMUPlanViewModelFactory(LocalContext.current)
-    )
+//    val mapviewModel : AAMUPlanViewModel = viewModel(
+//        factory = AAMUPlanViewModelFactory(LocalContext.current)
+//    )
     var topbarhide = remember { mutableStateOf(false) }
     Box(modifier = Modifier.fillMaxSize()){
         BottomSheetScaffold(
@@ -44,45 +51,63 @@ fun AAMUPlanHome(){
             content = {
                 Box(contentAlignment = Alignment.Center) {
                     Box(modifier = Modifier.fillMaxSize()) {
-                        KakaoMap(mapView = mapviewModel.mapView, PlanListener())
+//                        KakaoMap(mapView = mapviewModel.mapView, PlanListener())
                     }
                     if(!topbarhide.value) {
-                    ActionButton(mapviewModel,
-                        Modifier
-                            .statusBarsPadding()
-                            .align(Alignment.TopEnd),
-                        topbarhide,
-                        bottomSheetScaffoldState,
-                        coroutineScope)
-//                        ActionButton(
-//                            Modifier
-//                                .statusBarsPadding()
-//                                .align(Alignment.TopEnd),
-//                            topbarhide,
-//                            bottomSheetScaffoldState,
-//                            coroutineScope)
+//                    ActionButton(mapviewModel,
+//                        Modifier
+//                            .statusBarsPadding()
+//                            .align(Alignment.TopEnd),
+//                        topbarhide,
+//                        bottomSheetScaffoldState,
+//                        coroutineScope)
+                        ActionButton(
+                            Modifier
+                                .statusBarsPadding()
+                                .align(Alignment.TopEnd),
+                            topbarhide,
+                            bottomSheetScaffoldState,
+                            coroutineScope)
                     }
                 }
             },
             sheetContent = {
                 PlanBottomSheet()
             },
+            drawerBackgroundColor = orange700,
+            drawerGesturesEnabled = topbarhide.value,
+            drawerContent = {
+                Spacer(modifier = Modifier.height(56.dp))
+                SideContent() },
             scaffoldState = bottomSheetScaffoldState,
             sheetPeekHeight = 25.dp
         )
+        val lists : MutableList<Int> = arrayListOf()
+        for(i : Int in 1..50)
+            lists.add(i)
         if(topbarhide.value) {
-            SideContent()
-            TopAppBar() {
-                Spacer(modifier = Modifier.statusBarsPadding())
-                IconButton(onClick = {
-                    coroutineScope.launch { bottomSheetScaffoldState.drawerState.close() }
-                    topbarhide.value = false
-
-                }) {
-                    androidx.compose.material.Icon(Icons.Filled.ArrowBack, null)
+            if(bottomSheetScaffoldState.drawerState.isClosed) {
+                LazyColumn(
+                    modifier = Modifier
+                        .width(70.dp)
+                        .background(color = amber200)
+                        .padding(top = 56.dp),
+                    state = lazyListState
+                ) {
+                    itemsIndexed(items = lists,
+                        itemContent = { index, list ->
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp)
+                            ) {
+                                androidx.compose.material.Text(text = "Item " + list.toString())
+                            }
+                        }
+                    )
                 }
-                androidx.compose.material3.Text(text = "ㅎㅇㅎ")
             }
+            PlanDetails(bottomSheetScaffoldState,topbarhide,coroutineScope)
         }
     }
 
@@ -138,7 +163,7 @@ fun PlanBottomSheet(){
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun ActionButton(
-    mapviewModel : AAMUPlanViewModel,
+//    mapviewModel : AAMUPlanViewModel,
     modifier: Modifier,
     topbarhide : MutableState<Boolean>,
     bottomSheetScaffoldState: BottomSheetScaffoldState,
@@ -147,13 +172,10 @@ fun ActionButton(
     Box(modifier = modifier) {
         FloatingActionButton(
             onClick = {
-                mapviewModel.mapView.currentLocationTrackingMode=MapView.CurrentLocationTrackingMode.TrackingModeOff
-                mapviewModel.getRcentPlaces()
+//                mapviewModel.mapView.currentLocationTrackingMode=MapView.CurrentLocationTrackingMode.TrackingModeOff
+//                mapviewModel.getRcentPlaces()
                 topbarhide.value = true
-                coroutineScope.launch {
-                    bottomSheetScaffoldState.drawerState.open()
-                }
-
+                coroutineScope.launch { bottomSheetScaffoldState.drawerState.open() }
             },
             modifier = Modifier
                 .size(width = 50.dp, height = 50.dp)
@@ -171,38 +193,58 @@ fun ActionButton(
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun PlanDetails(
+//    mapviewModel : AAMUPlanViewModel,
+    scaffoldState : BottomSheetScaffoldState,
+    topbarhide : MutableState<Boolean>,
+    coroutineScope: CoroutineScope
+){
+    Column() {
+        Spacer(modifier = Modifier.statusBarsPadding())
+        TopAppBar() {
+            IconButton(onClick = {
+                coroutineScope.launch { scaffoldState.drawerState.close() }
+                topbarhide.value = false
+            }) {
+                androidx.compose.material.Icon(Icons.Filled.ArrowBack, null)
+            }
+            androidx.compose.material.Text(text = "ㅎㅇㅎ")
+        }
+    }
+}
+
 @Composable
 fun SideContent() {
-    Box(modifier = Modifier.fillMaxWidth()){
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp), horizontalArrangement = Arrangement
-                .SpaceBetween
-        ) {
-            androidx.compose.material.Text(text = "Item 1")
-            androidx.compose.material.Icon(imageVector = Icons.Default.List, contentDescription = "List")
-        }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp), horizontalArrangement = Arrangement
+            .SpaceBetween
+    ) {
+        androidx.compose.material.Text(text = "Item 1")
+        androidx.compose.material.Icon(imageVector = Icons.Default.List, contentDescription = "List")
+    }
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp), horizontalArrangement = Arrangement
-                .SpaceBetween
-        ) {
-            androidx.compose.material.Text(text = "Item 2")
-            androidx.compose.material.Icon(imageVector = Icons.Default.List, contentDescription = "List")
-        }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp), horizontalArrangement = Arrangement
+            .SpaceBetween
+    ) {
+        androidx.compose.material.Text(text = "Item 2")
+        androidx.compose.material.Icon(imageVector = Icons.Default.List, contentDescription = "List")
+    }
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp), horizontalArrangement = Arrangement
-                .SpaceBetween
-        ) {
-            androidx.compose.material.Text(text = "Item 3")
-            androidx.compose.material.Icon(imageVector = Icons.Default.List, contentDescription = "List")
-        }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp), horizontalArrangement = Arrangement
+            .SpaceBetween
+    ) {
+        androidx.compose.material.Text(text = "Item 3")
+        androidx.compose.material.Icon(imageVector = Icons.Default.List, contentDescription = "List")
     }
 }
 
