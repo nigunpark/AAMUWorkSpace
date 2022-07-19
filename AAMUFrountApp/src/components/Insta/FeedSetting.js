9a90636473dc0582d4b836a5cd1e37232801f578
jiyellie <106718961+jiyellie@users.noreply.github.com>
2022-07-19 22:14:45 +0900

    Update FeedSetting.js

--- AAMUFrountApp/src/components/Insta/FeedSetting.js
+++ AAMUFrountApp/src/components/Insta/FeedSetting.js
@@ -18,6 +18,7 @@ function FeedSetting({val,setMyImage}) {
     const [profileModal, setprofileModal] = useState(false);
     const [commentModal, setcommentModal] = useState(false);
 
+    let [userName] = useState('hacker');
     let [comment, setComment] = useState('');
     let [feedComments, setfeedComments] = useState([]); 
     let [isValid, setisValid] = useState(false); 
@@ -46,8 +47,20 @@ function FeedSetting({val,setMyImage}) {
     
     window.addEventListener("click", menuModalRef);
     
-
-
+    const CommentList = props => {
+        return(
+            <div className = 'userCommentBox'>
+                <p className="userName">{props.userName}</p>
+                <div className="userCommnet">{props.userComment}</div>
+                <p className="userHeart">
+                    {/* <FaHeart/> */}
+                </p>
+            </div>
+        )
+    }
+    function islike(){
+        
+    }
   return (
 <div>
     <div className="feeds-setting">  
@@ -100,8 +113,8 @@ function FeedSetting({val,setMyImage}) {
     <div className="feeds-contents">
         <div className="feeds-icons">
             <div className='heart-icon'>
-                {heart ?<i className="fa-solid fa-heart fa-2x"onClick={()=>{setHeart(false)}} style={{color:'red'}} />
-                :<i className="fa-regular fa-heart fa-2x"  onClick={()=>{setHeart(true)}}></i>}
+                {val.islike ?<i className="fa-solid fa-heart fa-2x"onClick={()=>{}} style={{color:'red'}} />
+                :<i className="fa-regular fa-heart fa-2x"  onClick={()=>{}}></i>}
             </div>
             <div>
                 <div className="talk-icon">
@@ -130,11 +143,8 @@ function FeedSetting({val,setMyImage}) {
             </svg> */}
         </div>
         <div className="feeds-like">
-            <img src="./img/bk.jpg" className="feeds-commentimage" alt="프사" />
-            <span><span className="like-bold">0hyun0hyun</span>님
-            <span className="like-bold">외
-             {val.likecount}
-             명</span>이 좋아합니다</span>
+            <span><span className="like-bold"><strong>좋아요</strong></span>
+            <span className="like-bold"> {val.likecount}명</span>이 좋아합니다</span>
         </div>
         <div className="feeds-title">
             <span className="comment-id">제목 : </span>
@@ -148,7 +158,7 @@ function FeedSetting({val,setMyImage}) {
                 </span>
                 <span> 
                     {val.content}
-                    </span>
+                </span>
         </div>
         <div className="feeds-writing">
             <span className="comment-id">
@@ -158,6 +168,17 @@ function FeedSetting({val,setMyImage}) {
                 {val.commuComment==null?"":val.commuComment.reply}
                 </span>
         </div>
+        {
+            feedComments.map((commentArr,i)=>{//feedComments에 담겨있을 댓글 값을 CommentList 컴포넌트에 담아서 가져온다
+                return(
+                    <CommentList//CommentList 컴포넌트는 반복적으로 추가되는 사용자 댓글 하나하나를 담고있는 박스
+                        userName={userName}//userName에는 위에서 'hacker'를 담은 값을 userComment에는 feedComments에 담겨있는 댓글 배열을 담는다
+                        userComment={commentArr}
+                        key={i}
+                    />
+                );
+            })
+        }
         <div className="feeds-comment">
         </div>
         <span className="time">
@@ -166,29 +187,37 @@ function FeedSetting({val,setMyImage}) {
     </div>
     <div className="comment">
         <input type="text"
-                className="typing-comment" 
+                className="inputComment" 
                 placeholder="댓글 달기..."
-                // onChange={(e)=>{
-                //     setComment(e.target.value);//댓글 창의 상태가 변할때마다 setComment를 통해 comment값을 바꿔준다
-                // }}
-                // onKeyUp={(e)=>{
-                //     e.target.value.length > 0//사용자가 키를 눌렀다 떼었을때 길이가 0을 넘는 값인지 유효성 검사 결과 값을 담는다
-                //     ? setisValid(true)
-                //     : setisValid(false);
-                // }}
-                // value={comment}
+                onChange={(e)=>{
+                    setComment(e.target.value);//댓글 창의 상태가 변할때마다 setComment를 통해 comment값을 바꿔준다
+                }}
+                onKeyUp={(e)=>{
+                    e.target.value.length > 0//사용자가 키를 눌렀다 떼었을때 길이가 0을 넘는 값인지 유효성 검사 결과 값을 담는다
+                    ? setisValid(true)
+                    : setisValid(false);
+                }}
+                value={comment}
                 />
 
         <button 
-                className="comment-button" 
-                type="button">
+                className={//클래스명을 comment창의 글자 길에 따라서 다르게 주면서 버튼색에 css디자인을 줄 수 있음
+                    comment.length > 0
+                    ? 'submitCommentActive'
+                    : 'submitCommentInactive'
+                }
+                onClick={post}//클릭하면 위서 선언한 post함수를 실행하여 feedComments에 담겨서 re-rendering 된 댓글창을 확인할 수 있다
+                disabled={isValid ? false : true}//사용자가 아무것도 입력하지 않았을 경우 게시를 할 수 없도록
+                type="button" >
                     게시
         </button>
+       
     </div>
     </div> 
 
     </div> 
   )
+  
 }
 
 const Container1 = styled.div`
