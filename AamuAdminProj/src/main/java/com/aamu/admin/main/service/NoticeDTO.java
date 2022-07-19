0c61396669afac981267b14fd09cf4aa4bec8577
woojin <100190802+woozinius@users.noreply.github.com>
2022-07-19 09:57:22 +0900

    Create NoticeDTO.java

---
+++ AamuAdminProj/src/main/java/com/aamu/admin/main/service/NoticeDTO.java
@@ -0,0 +1,14 @@
+package com.aamu.admin.main.service;
+
+public class NoticeDTO {
+	
+	/*
+	NNO number NOT NULL,
+	TITLE nvarchar2(100) NOT NULL,
+	CONTENT nvarchar2(2000) NOT NULL,
+	NOTICEDATE date DEFAULT SYSDATE NOT NULL,
+	NCOUNT number DEFAULT 0 NOT NULL,
+	PRIMARY KEY (NNO)
+	*/
+
+}
