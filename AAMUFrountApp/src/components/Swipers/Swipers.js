e51fd62c70c5bcb2f289d5fff4bd1b93196df9fe
MoZZANG <kkm0938@naver.com>
2022-07-22 20:35:07 +0900

    Update Swipers.js

--- AAMUFrountApp/src/components/Swipers/Swipers.js
+++ AAMUFrountApp/src/components/Swipers/Swipers.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SwipersItem from "./SwipersItem";
 import "./Swipers.css";
 import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from "swiper";
@@ -6,8 +6,12 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css"; //basic
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-
+import axios from "axios";
 const Swipers = () => {
+  const [forSwiper, setForSwiper] = useState([]);
+  useEffect(() => {
+    get(setForSwiper);
+  }, []);
   return (
     <div className="Swipers">
       <div className="swiper__container">
@@ -28,38 +32,19 @@ const Swipers = () => {
               pagination={{ clickable: true }}
               scrollbar={{ draggable: true }}
             >
-              <SwiperSlide>
-                <SwipersItem
-                  src="/images/img-9.jpg"
-                  text="Explore the hidden waterfall deep inside the Amazon Jungle"
-                  label="Adventure"
-                  path="/"
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <SwipersItem
-                  src="/images/img-2.jpg"
-                  text="Explore the hidden waterfall deep inside the Amazon Jungle"
-                  label="Adventure"
-                  path="/"
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <SwipersItem
-                  src="/images/img-3.jpg"
-                  text="Explore the hidden waterfall deep inside the Amazon Jungle"
-                  label="Adventure"
-                  path="/"
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <SwipersItem
-                  src="/images/img-4.jpg"
-                  text="Explore the hidden waterfall deep inside the Amazon Jungle"
-                  label="Adventure"
-                  path="/"
-                />
-              </SwiperSlide>
+              {forSwiper.map((val, i) => {
+                return (
+                  <SwiperSlide>
+                    <SwipersItem
+                      style={{ fontSize: "13px" }}
+                      src={val.bigimage}
+                      text={val.title}
+                      label={`${getLocalName(val.areacode)}`}
+                      path="/"
+                    />
+                  </SwiperSlide>
+                );
+              })}
             </Swiper>
           </ul>
         </div>
@@ -68,4 +53,70 @@ const Swipers = () => {
   );
 };
 
+function get(setForSwiper) {
+  // let token = sessionStorage.getItem("token");
+  axios
+    .get("/aamurest/main/mainelement")
+    .then((resp) => {
+      setForSwiper(Object.values(resp.data)[0].places);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function getLocalName(areacode) {
+  switch (areacode) {
+    case 1:
+      return "서울";
+
+    case 2:
+      return "인천";
+
+    case 3:
+      return "대전";
+
+    case 4:
+      return "대구";
+
+    case 5:
+      return "광주";
+
+    case 6:
+      return "부산";
+
+    case 7:
+      return "부산";
+
+    case 8:
+      return "세종";
+
+    case 31:
+      return "경기도";
+
+    case 32:
+      return "강원도";
+
+    case 33:
+      return "충청북도";
+
+    case 34:
+      return "충청남도";
+
+    case 35:
+      return "경상북도";
+
+    case 36:
+      return "경상남도";
+
+    case "37":
+      return "전라북도";
+
+    case 38:
+      return "전라남도";
+
+    case 39:
+      return "제주도";
+  }
+}
 export default Swipers;
