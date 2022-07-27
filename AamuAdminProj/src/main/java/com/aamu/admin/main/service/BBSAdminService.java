780f3811259cde749a4fcb2d66725b109e1ec4d6
Uuser29 <jihyunn1129@naver.com>
2022-07-28 00:42:27 +0900

    admin

--- AamuAdminProj/src/main/java/com/aamu/admin/main/service/BBSAdminService.java
+++ AamuAdminProj/src/main/java/com/aamu/admin/main/service/BBSAdminService.java
@@ -2,19 +2,25 @@ package com.aamu.admin.main.service;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 public interface BBSAdminService {
 	
 	//전체 게시글 목록
+	ListPagingData<BBSAdminDTO> bbsSelectList(Map map,HttpServletRequest req,int nowPage);
 	
 	//전체 게시글 수
 	int bbsGetTotalRecordCount(Map map);
+	
+	//게시글 하나 보기
+	BBSAdminDTO bbsAdimSelectOne(Map map);
 	
 	//게시글 삭제
 	int bbsDelete(Map map);
 	
 	//전체 리뷰 목록
-	
+	ListPagingData<ReviewAdminDTO> reviewSelectList(Map map,HttpServletRequest req,int nowPage);
 	
 	//리뷰 삭제
-	
+	int reviewDelete(Map map);
 }
