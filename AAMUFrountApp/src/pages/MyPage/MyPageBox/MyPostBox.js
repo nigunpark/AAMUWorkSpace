0b0387ebd10f7cdf20bf8a3bb08960cd2c407845
eungGit <106792110+eungGit@users.noreply.github.com>
2022-07-28 20:18:56 +0900

    Update MyPostBox.js

--- AAMUFrountApp/src/pages/MyPage/MyPageBox/MyPostBox.js
+++ AAMUFrountApp/src/pages/MyPage/MyPageBox/MyPostBox.js
@@ -3,31 +3,40 @@ import React, { useCallback, useEffect, useState } from 'react'
 import styled from 'styled-components';
 
 const MyPostBox = ({selectRbn}) => {
-    const [detailPostData, setDetailPostData] = useState();
+    const [detailPostData, setDetailPostData] = useState({});
+    const [detailTitle, setDetailTitle] = useState('');
+    const [detailRoute, setDetailRoute] = useState([]);
+
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
     const [tag, setTag] = useState();
-    const [detailRbn, setDetailRbn] = useState();
+    const [detailRbn, setDetailRbn] = useState(0);
+    const themeid = 16;
     let [writeTag, setWriteTag] = useState([]);
 
     //--------------------------------이미지 시작--------------------------------
     const [showImages, setShowImages] = useState([]);
+    const [showImagesFile, setShowImagesFile] = useState([]);
 
     //이미지 등록
     const handleAddImages = (e) => {
         const imageLists = e.target.files;
         let imageUrlLists = [...showImages];
+        let imgs = [...showImagesFile];
 
         for (let i = 0; i < imageLists.length; i++) {
-        const currentImageUrl = URL.createObjectURL(imageLists[i]);
-        imageUrlLists.push(currentImageUrl);
+          const currentImageUrl = URL.createObjectURL(imageLists[i]); //이거 안됨..
+          imageUrlLists.push(currentImageUrl);
+
+          imgs.push(imageLists[i]); // 한개씩 첨부했을때 post로 잘 넘어가게
         }
 
         if (imageUrlLists.length > 6) { //사진 6개 제한
-        imageUrlLists = imageUrlLists.slice(0, 6);
+          imageUrlLists = imageUrlLists.slice(0, 6);
         }
 
         setShowImages(imageUrlLists);
+        setShowImagesFile(imgs);
     };
 
   const handleDeleteImage = (id) => { //등록한 사진 삭제
@@ -37,51 +46,64 @@ const MyPostBox = ({selectRbn}) => {
 
     const canSubmit = useCallback(() => {
         return content !== "" && title !== "";
-      }, [title, content]);
+      }, [title, content]); //제목, 내용 입력 안되면 공유버튼 비활성화
 
-    let test = [1,2,3,4];
-
-
-    useEffect(()=>{
+    useEffect(async ()=>{
+      try{
         let token = sessionStorage.getItem("token");
-        axios.get('/aamurest/planner/selectonemap',{
+        
+        let resp = await axios.get('/aamurest/planner/selectonemap',{
             params:{
-                // id:sessionStorage.getItem('username'),
                 rbn:selectRbn
             },
             headers: {
                 Authorization: `Bearer ${token}`,
             }
-        }).then((resp)=>{
-            console.log('글 작성 페이지에서 상세경로 데이터 확인 : ',resp.data);
-            setDetailPostData(resp.data);
-            setDetailRbn(resp.data.rbn);
-        }).catch((error)=>{
-            console.log((error) => console.log("상세경로 가져오기 실패", error));
-        });
+
+        })
+        console.log('글 작성 페이지에서 상세경로 데이터 확인 : ',resp.data);
+        setDetailPostData(resp.data);
+        setDetailRbn(resp.data.rbn);
+        setDetailTitle(resp.data.title);
+        setDetailRoute(Object.entries(resp.data.routeMap));
+      }
+      catch(error){
+        console.log((error) => console.log("상세경로 가져오기 실패", error));
+      };
         
-      },[]);
+    },[]);
 
+    
+    console.log('detailPostData :', detailPostData);
+    console.log('detailRoute :', detailRoute);
 
-      console.log('detailRbn :', detailRbn);
-
+    let test = [1,2,3,4];
   return (
     <div className="MyWrite-container">
 
       <div className="write-box plan">
-        <div className="plan-title">여행경로</div>
+        <div className="plan-title">{detailTitle} 여행경로</div>
 
         <div style={{border:'1.5px solid #edf2f4'}}>
           {
-            test.map((val, idx)=>{
+            detailRoute.map((route, idx)=>{
               return(
-                <div className="detail-plan">
-                  <span className="paln-date">{val} 일차 20xx-xx-xx x요일</span>
-                  <div>00:00 ~ 00:00</div>
-                  <div className="plan-region">제주공항</div>
-
-                  <div>00:00 ~ 00:00</div>
-                  <div className="plan-region">제주공항</div>
+                <div key={idx} className="detail-plan">
+                  <span className="paln-date">{idx+1} 일차 20xx-xx-xx x요일</span>
+                  {
+                    route[1].map((val, i)=>{
+                      let firstTime = (val.starttime)/1000/60/60;
+                      console.log('val :',firstTime);
+                      console.log('-------------',i);
+                      return(
+                        <div key={i}>
+                          <div>{firstTime} ~ 00:00</div>
+                          <div className="plan-region"></div>
+                          <hr/>
+                        </div>
+                      )
+                    })//내부 map
+                  }
                 </div>
               )
             })
@@ -155,8 +177,8 @@ const MyPostBox = ({selectRbn}) => {
             className="navbar-btn"
             type="button"
             onClick={()=>{
-              let write = uploadFile(showImages);
-              bordWrite(write, title, content, tag, writeTag, setWriteTag, detailRbn);
+              let write = uploadFile(showImagesFile);
+              bordWrite(write, title, content, tag, writeTag, setWriteTag, detailRbn, themeid);
             }}>공유하기</button>
           ) : (
             <button  type="button" disabled>제목과 내용을 모두 입력하세요</button>
@@ -172,22 +194,17 @@ const MyPostBox = ({selectRbn}) => {
 function uploadFile(showImages){//이미지 업로드
     let formData = new FormData(); // formData 객체를 생성한다.
     for (let i = 0; i < showImages.length; i++) { 
-      formData.append("photo", showImages[i]); // 반복문을 활용하여 파일들을 formData 객체에 추가한다
+      formData.append("multifiles", showImages[i]); // 반복문을 활용하여 파일들을 formData 객체에 추가한다
     }
     return formData;
   };
-  function bordWrite(write, title, content, tag, writeTag, setWriteTag, detailRbn){
-  
-    // setWriteTag(tag.split('#'));
-    // writeTag.splice(0,1);
-    // console.log('writeTag :', writeTag);
+  function bordWrite(write, title, content, tag, writeTag, setWriteTag, detailRbn, themeid){
   
     write.append('id', sessionStorage.getItem('username'));
     write.append('title', title);
     write.append('content', content);
     write.append('rbn', detailRbn);
-    write.append('themeid', 'themeHard');
-    // write.append('writeTag', writeTag);
+    write.append('themeid', themeid);
   
     let token = sessionStorage.getItem("token");
     axios.post('/aamurest/bbs/edit',write, {
