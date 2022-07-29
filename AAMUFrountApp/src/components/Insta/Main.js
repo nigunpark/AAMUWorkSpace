2427e568fd70d6bf366234198dc8b57e0cb704ae
MoZZANG <kkm0938@naver.com>
2022-07-29 21:44:47 +0900

    Update Main.js

--- AAMUFrountApp/src/components/Insta/Main.js
+++ AAMUFrountApp/src/components/Insta/Main.js
@@ -5,58 +5,73 @@ import FeedSetting from "./FeedSetting.js";
 import User from "./User.js";
 import axios from "axios";
 import Spinner from "./Spinner";
-function Main({searchb, setSearchb, inputValue, setinputValue}) {
 
-  const [list,setlist] = useState([]);
-  const [forReRender, setForReRender] = useState(false)
-  function feedList(){//백이랑 인스타 리스드를 뿌려주기 위한 axios
+function Main({ searchb, setSearchb, inputValue, setinputValue }) {
+  const [list, setlist] = useState([]);
+  const [forReRender, setForReRender] = useState(false);
+  const [showChat, setShowChat] = useState(false);
+  function feedList() {
+    //백이랑 인스타 리스드를 뿌려주기 위한 axios
     let token = sessionStorage.getItem("token");
-    axios.get('/aamurest/gram/selectList',{
-      headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          params:{
-            id:sessionStorage.getItem('username')
-          }
-    })
-    .then((resp) => {
-      console.log(resp.data)
-      setlist(resp.data);
+    axios
+      .get("/aamurest/gram/selectList", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: {
+          id: sessionStorage.getItem("username"),
+        },
+      })
+      .then((resp) => {
+        console.log(resp.data);
+        setlist(resp.data);
       })
       .catch((error) => {
         console.log(error);
       });
   }
- 
-  useEffect(()=>{
-    feedList()
-    setinputValue('')
-  },[])
 
-  const spinner = document.querySelector('.spinner');  
-	const [loading, setloading] = useState(false )
-  
+  useEffect(() => {
+    feedList();
+    setinputValue("");
+  }, []);
+
+  const spinner = document.querySelector(".spinner");
+  const [loading, setloading] = useState(false);
+
   return (
     <div className="main">
-          
-
       {/* <div className="margin-top">  
           <Story></Story>   
         </div> */}
       <div className="margin-value">
-        <div className="main-left" style={{display:'flex',flexDirection:'column'}}>
-        {loading &&
-          <Spinner/>
-        }
-        {
-          list.map((val,i)=>{
-            return <FeedSetting val={val} setlist={setlist} forReRender={forReRender} setForReRender={setForReRender} setloading={setloading}/>
-          })
-        }
-        {/* <FeedSetting></FeedSetting> */}
+        <div className="main-left" style={{ display: "flex", flexDirection: "column" }}>
+          {loading && <Spinner />}
+          {list.map((val, i) => {
+            return (
+              <FeedSetting
+                val={val}
+                setlist={setlist}
+                forReRender={forReRender}
+                setForReRender={setForReRender}
+                setloading={setloading}
+                setShowChat={setShowChat}
+                showChat={showChat}
+              />
+            );
+          })}
+          {/* <FeedSetting></FeedSetting> */}
         </div>
         <div className="main-right">
-          <User inputValue={inputValue} setinputValue={setinputValue} setlist={setlist} setloading={setloading} searchb={searchb} setSearchb={setSearchb}></User>
+          <User
+            inputValue={inputValue}
+            setinputValue={setinputValue}
+            setlist={setlist}
+            setloading={setloading}
+            searchb={searchb}
+            setSearchb={setSearchb}
+            showChat={showChat}
+          ></User>
         </div>
       </div>
     </div>
