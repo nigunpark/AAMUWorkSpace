994f3717a2878575e56192600e2e8a211c21db74
eungGit <106792110+eungGit@users.noreply.github.com>
2022-07-30 14:30:21 +0900

    Update MyInstaBox.jsx

--- AAMUFrountApp/src/pages/MyPage/MyPageBox/MyInstaBox.jsx
+++ AAMUFrountApp/src/pages/MyPage/MyPageBox/MyInstaBox.jsx
@@ -7,6 +7,10 @@ import { Avatar } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
 import { SwiperSlide, Swiper } from "swiper/react";
+import { A11y, Autoplay, Navigation, Pagination, Scrollbar } from "swiper";
+import { Button } from "bootstrap";
+import { escapeSelector } from "jquery";
+import MySearchModal from "./MySearchModal";
 
 const MyInstaBox = () => {
   const [showImages, setShowImages] = useState([]); //이미지
@@ -59,6 +63,91 @@ const MyInstaBox = () => {
       });
   }, []);
 
+  const [myImage, setMyImage] = useState([]);
+  const [myImagefile, setMyImageFile] = useState([]);
+  const [hide, setHide] = useState(false);
+  const [showSearch, setshowSearch] = useState(false);
+  const [search, setSearch] = useState([]);
+  const [inputValue, setinputValue] = useState("");
+  const [hasText, setHasText] = useState(false);
+
+  let titleRef = useRef();
+  let textareaRef = useRef();
+  let searchRef = useRef();
+
+  const addImage = (e) => {
+    const nowSelectImageList = e.target.files; //한번에 받은 파일 리스트(object)
+    const nowImageURLList = [...myImage]; //현재 myImage복사하고
+    for (let i = 0; i < nowSelectImageList.length; i += 1) {
+      //nowSelectImageList object를 i를 이용해서 돌리면서
+      const nowImageURL = URL.createObjectURL(nowSelectImageList[i]);
+      //미리보기 가능하게 변수화
+      nowImageURLList.push(nowImageURL);
+      //복사한 myImage에 추가
+    }
+    if (nowImageURLList.length > 5) {
+      alert("사진은 5장 이하로만 가능합니다!");
+      setHide(false);
+      nowImageURLList = nowImageURLList.slice(0, 5);
+    }
+    setMyImage(nowImageURLList);
+    setMyImageFile(nowSelectImageList);
+
+    //myImage원본에 덮어씌우기
+  };
+
+  function fn_checkByte(obj) {
+    //textarea입력한 글자 count 및 글자 수 제한
+    const maxByte = 1000; //최대 100바이트
+    const text_val = obj.target.value; //입력한 문자
+    const text_len = text_val.length; //입력한 문자수
+
+    let totalByte = 0;
+    for (let i = 0; i < text_len; i++) {
+      const each_char = text_val.charAt(i);
+      const uni_char = escapeSelector(each_char); //유니코드 형식으로 변환
+      if (uni_char.length > 4) {
+        // 한글 : 2Byte
+        totalByte += 2;
+      } else {
+        // 영문,숫자,특수문자 : 1Byte
+        totalByte += 1;
+      }
+    }
+
+    if (totalByte > maxByte) {
+      alert("최대 1000Byte까지만 입력가능합니다.");
+      document.getElementById("nowByte").innerText = totalByte;
+      document.getElementById("nowByte").style.color = "red";
+    } else {
+      document.getElementById("nowByte").innerText = totalByte;
+      document.getElementById("nowByte").style.color = "green";
+    }
+  }
+
+  function searchWord(e, setSearch) {
+    //위치 지정을 위한 백에게 받는 axios
+    let val = e.target.value;
+    if (e.keyCode != 13) return;
+    let token = sessionStorage.getItem("token");
+    axios
+      .get("/aamurest/gram/place/selectList", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: {
+          searchWord: val,
+        },
+      })
+      .then((resp) => {
+        // console.log(resp.data);
+        setSearch(resp.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <MyProfileContainer>
       <FirstLine>
@@ -68,68 +157,217 @@ const MyInstaBox = () => {
         {/* {showNext ?  */}
         <Nextbtn
           onClick={() => {
-            // edit(val,setlist,setShowWrite,titleRef,textareaRef,searchRef,search)
-            // seteditModal(false)
+            edit();
+            // val, //이게 이미지인거같은데
+            // setlist,
+            // setShowWrite,
+            // titleRef,
+            // textareaRef,
+            // searchRef,
+            // search
+            // seteditModal(false);
             // feedList(setlist)
           }}
         >
           <FontAwesomeIcon icon={faPaperPlane} size="2x" />
         </Nextbtn>
       </FirstLine>
-
-      {/* <Body>
-          <form className='picfileframe'  encType='multipart/form-data'>
-            <input  
-              id="input-file"
-              type="file" 
-              multiple
-              accept="image/*"
-              // 클릭할 때 마다 file input의 value를 초기화 하지 않으면 버그가 발생할 수 있다
-              // 사진 등록을 두개 띄우고 첫번째에 사진을 올리고 지우고 두번째에 같은 사진을 올리면 그 값이 남아있음!
-              onClick={(e) => e.target.value = null}
-              // ref={refParam => inputRef = refParam}
-              style={{display: "none" , width:'100%',height:'100%'}}
-            />
-            <div className="previewPic1">
-              <ul>
-                <Swiper
-                  className="swiperContainer1"
-                  // modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-                  spaceBetween={10}
-                  slidesPerView={1}
-                  // navigation
-                  autoplay={{ delay: 2500 }}
-                  loop={true}
-                  pagination={{ clickable: true }}
-                  scrollbar={{ draggable: true }}
-                  >
-                  {
-                    val.photo.map((image,i)=>{
-                      return(
-                          <SwiperSlide>
-                            <li>
-                              <img className='divimage1' alt="sample" src={image}/>
-                            </li>
-                          </SwiperSlide>
-                        )
-                      })
-                  }
-                </Swiper>
-              </ul>
-            </div>
-            <label 
-              className="rweet_file_btn" 
-              onClick={()=>{
-                // setHide(!hide)
-              }}
-              htmlFor="input-file"          
+      <Body>
+        <form
+          className="picfileframe"
+          encType="multipart/form-data"
+          style={{ border: "1px solid black" }}
+        >
+          <input
+            id="input-file"
+            type="file"
+            multiple
+            accept="image/*"
+            onChange={addImage}
+            // 클릭할 때 마다 file input의 value를 초기화 하지 않으면 버그가 발생할 수 있다
+            // 사진 등록을 두개 띄우고 첫번째에 사진을 올리고 지우고 두번째에 같은 사진을 올리면 그 값이 남아있음!
+            onClick={(e) => {
+              e.stopPropagation();
+              e.target.value = null;
+            }}
+            // ref={refParam => inputRef = refParam}
+            style={{ display: "none", width: "100%", height: "100%" }}
+          />
+          {/* {myImage.map((images,i)=>(
+                                   <img className='divimage' alt="sample" src={images}/>
+                             
+                          ))}  */}
+          <div className="previewPic1">
+            <ul>
+              <Swiper
+                // className="swiperContainer1"
+                modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+                spaceBetween={10}
+                slidesPerView={1}
+                // navigation
+                autoplay={{ delay: 2500 }}
+                loop={true}
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
               >
-            </label>  
-          </form>
-        </Body> */}
+                {myImage.map((image, i) => {
+                  return (
+                    <SwiperSlide>
+                      <li>
+                        <img className="divimage1" alt="sample" src={image} />
+                      </li>
+                    </SwiperSlide>
+                  );
+                })}
+              </Swiper>
+            </ul>
+          </div>
+          <label
+            className="rweet_file_btn"
+            onClick={() => {
+              setHide(!hide);
+            }}
+            htmlFor="input-file"
+            onChange={addImage}
+          >
+            {hide ? null : <button>컴퓨터에서 선택</button>}
+          </label>
+          {/* onClick={() => inputRef.click()} */}
+        </form>
+
+        {/* {showNext ?  */}
+        <div className="my-side">
+          <div className="my-title-profile">
+            <img
+              src="'/img/bk.jpg ' ?? '/images/user.jpg'"
+              alt="프사"
+              onError={(e) => {
+                e.stopPropagation();
+                e.target.src = "/images/user.jpg";
+              }}
+            />
+            <span className="uploadname">
+              {sessionStorage.getItem("username")}
+            </span>
+          </div>
+          <div style={{ display: "flex" }}>
+            <span
+              style={{
+                fontWeight: "bold",
+                marginLeft: "10px",
+                width: "50px",
+              }}
+            >
+              제목 :{" "}
+            </span>
+            <input ref={titleRef} type="text" placeholder="제목을 입력하세요" />
+          </div>
+          <div>
+            <textarea
+              ref={textareaRef}
+              // className="form-control"
+              id="textArea_byteLimit"
+              name="textArea_byteLimit"
+              onKeyUp={(e) => fn_checkByte(e)}
+              rows="8"
+              placeholder="문구입력..."
+              style={{
+                border: "none",
+                resize: "none",
+                fontSize: "16px",
+                fontFamily: "normal",
+                outline: "none",
+                paddingTop: "5px",
+                marginLeft: "10px",
+                width: "90%",
+                position: "relative",
+              }}
+            ></textarea>
+          </div>
+          <div
+            style={{
+              borderBottom: "0.1px solid #c0c0c0",
+              width: "97%",
+              height: "27px",
+            }}
+          >
+            <sup
+              style={{ float: "right", paddingRight: "15px", color: "#c0c0c0" }}
+            >
+              (<span id="nowByte">0</span>/1000bytes)
+            </sup>
+          </div>
+          <div
+            className="uploadLocation"
+            onClick={() => {
+              setshowSearch(!showSearch);
+            }}
+          >
+            <input
+              onKeyUp={(e) => searchWord(e, setSearch)}
+              value={inputValue}
+              onChange={(e) => {
+                e.stopPropagation();
+                setinputValue(e.target.value);
+                setHasText(true);
+              }}
+              placeholder="위치 추가"
+              type="text"
+              ref={searchRef}
+            />
+            {hasText ? (
+              <MySearchModal
+                search={search}
+                setHasText={setHasText}
+                setinputValue={setinputValue}
+              />
+            ) : null}
+            <i className="fa-solid fa-location-dot"></i>
+          </div>
+        </div>
+      </Body>
     </MyProfileContainer>
   );
 };
+function edit(
+  val,
+  setlist,
+  setShowWrite,
+  titleRef,
+  textareaRef,
+  searchRef,
+  search
+) {
+  //새 게시물 업로드를 위한 axios
+  let searched = search.find((val, i) => {
+    return val.TITLE === searchRef.current.value;
+  });
+
+  let token = sessionStorage.getItem("token");
+  axios
+    .put(
+      "/aamurest/gram/edit",
+      {
+        lno: val.lno,
+        ctitle: titleRef.current.value,
+        content: textareaRef.current.value,
+        contentid: searched.CONTENTID,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+    .then((resp) => {
+      console.log(resp.data);
+      setShowWrite(resp.data);
+      // feedList(setlist);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
 function uploadFile(showImages) {
   //이미지 업로드
@@ -163,7 +401,7 @@ const MyProfileContainer = styled.div`
   display: grid;
   width: 100%;
   // grid-template-columns: 520px 500px;
-  grid-template-rows: 50px 600px;
+  grid-template-rows: 40px 590px;
   // gap: 10px;
   // border: solid 1px black;
   font-size: 18px;
@@ -183,8 +421,10 @@ const Nextbtn = styled.button`
   font-weight: bold;
 `;
 const Body = styled.div`
-  display: flex;
-  height: 100%;
+  display: grid;
+  grid-template-columns: 70% 30%;
+  height: 570px;
+  border: 1px solid red;
 `;
 
 const MyUpdateImg = styled.div`
