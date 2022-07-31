82ce012770857db194257dd68a60d67f50a20bcd
nigunpark <107670168+nigunpark@users.noreply.github.com>
2022-07-31 23:23:57 +0900

    update android

--- AAMUAndroidApp/app/src/main/java/com/aamu/aamuandroidapp/fragment/main/sub/RouteBBSFragment.kt
+++ AAMUAndroidApp/app/src/main/java/com/aamu/aamuandroidapp/fragment/main/sub/RouteBBSFragment.kt
@@ -5,15 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import com.aamu.aamuandroidapp.R
+import com.aamu.aamuandroidapp.components.routebbs.RouteBBSHome
 
 class RouteBBSFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_route_bbs, container, false)
+    ): View = ComposeView(inflater.context).apply {
+
+        setContent {
+            RouteBBSHome()
+        }
     }
 }
