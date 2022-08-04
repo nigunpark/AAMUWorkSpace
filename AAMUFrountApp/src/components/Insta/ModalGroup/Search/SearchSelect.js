128370829632d082831e193fec34a2dbfa9a199f
jiyellie <106718961+jiyellie@users.noreply.github.com>
2022-08-04 21:56:12 +0900

    Update SearchSelect.js

--- AAMUFrountApp/src/components/Insta/ModalGroup/Search/SearchSelect.js
+++ AAMUFrountApp/src/components/Insta/ModalGroup/Search/SearchSelect.js
@@ -6,7 +6,7 @@ function SearchSelect({setSearchb}) {
             <option defaultValue={'DEFAULT'} value="choice">선택</option>
             <option value="ctitle">제목</option>
             <option value="id">아이디</option>
-            <option value="tag">태그</option>
+            <option value="tname">태그</option>
         </select>
   )
 }
