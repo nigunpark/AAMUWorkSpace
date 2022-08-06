8bf155ceca92e017b246e41c8941809c22011c0a
MoZZANG <kkm0938@naver.com>
2022-08-06 18:53:31 +0900

    Update Forum.js

--- AAMUFrountApp/src/pages/Forum/Forum.js
+++ AAMUFrountApp/src/pages/Forum/Forum.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Notice from "./Notice/Notice";
 import Content from "./Content/Content";
+import { useLocation } from "react-router-dom";
 
 const Forum = () => {
   return (
