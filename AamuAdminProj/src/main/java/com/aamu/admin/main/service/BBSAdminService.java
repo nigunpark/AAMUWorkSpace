5141d0dd51a45b6dc00da63c193e0bf48159d06b
Uuser29 <jihyunn1129@naver.com>
2022-08-08 16:54:23 +0900

    bbs사진추가

--- AamuAdminProj/src/main/java/com/aamu/admin/main/service/BBSAdminService.java
+++ AamuAdminProj/src/main/java/com/aamu/admin/main/service/BBSAdminService.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 public interface BBSAdminService {
 	
-	//전체 게시글 목록
+	//게시글 목록
 	ListPagingData<BBSAdminDTO> bbsSelectList(Map map,HttpServletRequest req,int nowPage);
 	
 	//전체 게시글 수
@@ -15,7 +15,7 @@ public interface BBSAdminService {
 	//게시글 삭제
 	int bbsDelete(Map map);
 	
-	//전체 리뷰 목록
+	//리뷰 목록
 	ListPagingData<ReviewAdminDTO> reviewSelectList(Map map,HttpServletRequest req,int nowPage);
 	
 	//전체 리뷰 수
