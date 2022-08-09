d1dac50b61f238ce121beefecbfe8982a98e9fad
MoZZANG <kkm0938@naver.com>
2022-08-09 17:22:56 +0900

    Update LeftPlanSide.js

--- AAMUFrountApp/src/components/LeftPlanSide/LeftPlanSide.js
+++ AAMUFrountApp/src/components/LeftPlanSide/LeftPlanSide.js
@@ -10,15 +10,18 @@ import { faClock } from "@fortawesome/free-regular-svg-icons";
 import { useDispatch } from "react-redux";
 import {
   addArrInForJangso,
+  addArrInForRestaurant,
   addAtimeArrForJangso,
   addMonthNDate,
   addWeather,
   changeLnfM,
   changeSaveDaysRedux,
   changeShowWhichModal,
+  delAllPickMazzip,
   deleteAllPickJanso,
   deletePickJangso,
   delOneSaveDaysRedux,
+  delPickMazzip,
   minSetter,
   resetMonthNDate,
   setInitForMin,
@@ -47,6 +50,7 @@ const LeftPlanSide = ({ currPosition, setIsLoading }) => {
   });
   let suksoRef = useRef();
   let jangsoRef = useRef();
+  let restaurantRef = useRef();
 
   useEffect(() => {
     //WholeMapLocalData.js에서 현재클릭해서 들어온 지역명과 같은 지역명 찾기
@@ -59,12 +63,18 @@ const LeftPlanSide = ({ currPosition, setIsLoading }) => {
     setAppearCalendar(true);
   }, []);
   useEffect(() => {
-    if (reduxState.showWhichModal === true) {
+    if (reduxState.showWhichModal === 1) {
       suksoRef.current.classList.add("lps__type-btn-picked");
       jangsoRef.current.classList.remove("lps__type-btn-picked");
+      restaurantRef.current.classList.remove("lps__type-btn-picked");
+    } else if (reduxState.showWhichModal === 2) {
+      suksoRef.current.classList.remove("lps__type-btn-picked");
+      jangsoRef.current.classList.remove("lps__type-btn-picked");
+      restaurantRef.current.classList.add("lps__type-btn-picked");
     } else {
       suksoRef.current.classList.remove("lps__type-btn-picked");
       jangsoRef.current.classList.add("lps__type-btn-picked");
+      restaurantRef.current.classList.remove("lps__type-btn-picked");
     }
   }, [reduxState.showWhichModal]);
   let dispatch = useDispatch();
@@ -113,22 +123,37 @@ const LeftPlanSide = ({ currPosition, setIsLoading }) => {
             className="leftPlanSide__pickLocal__type-btn sukbak"
             ref={suksoRef}
             onClick={() => {
-              dispatch(changeShowWhichModal(true));
+              dispatch(changeShowWhichModal(1));
             }}
           >
             숙소
           </span>
           <span
+            className="leftPlanSide__pickLocal__type-btn sukbak"
+            ref={restaurantRef}
+            onClick={() => {
+              dispatch(changeShowWhichModal(2));
+            }}
+          >
+            맛집
+          </span>
+          <span
             className="leftPlanSide__pickLocal__type-btn jangso lps__type-btn-picked"
             ref={jangsoRef}
             onClick={() => {
-              dispatch(changeShowWhichModal(false));
+              dispatch(changeShowWhichModal(3));
             }}
           >
             장소
           </span>
         </div>
-        {reduxState.showWhichModal ? <SukSoMadal /> : <JangSoModal />}
+        {reduxState.showWhichModal === 1 ? (
+          <SukSoMadal />
+        ) : reduxState.showWhichModal === 2 ? (
+          <MazzipModal />
+        ) : (
+          <JangSoModal />
+        )}
       </div>
     </div>
   );
@@ -248,6 +273,52 @@ function JangSoModal() {
   );
 }
 
+function MazzipModal() {
+  let reduxState = useSelector((state) => {
+    return state;
+  });
+  let dispatch = useDispatch();
+
+  return (
+    <div className="jangSoModal__container">
+      <div className="jangSoModal__counter__container">
+        <span className="jangSoModal__counter">{reduxState.arrForPickMazzip.length}</span>
+        <span>
+          (총{reduxState.leftSideTimeSetter}시간{reduxState.leftSideMinSetter}
+          분)
+        </span>
+      </div>
+      <div
+        className="jangSoModal__allDel-btn"
+        onClick={() => {
+          {
+            reduxState.arrForPickMazzip.map((local, index) => {
+              dispatch(addArrInForRestaurant(local));
+            });
+          }
+          dispatch(delAllPickMazzip([]));
+          dispatch(setInitForTime(0));
+          dispatch(setInitForMin(0));
+        }}
+      >
+        맛집전체삭제
+      </div>
+      {reduxState.arrForPickMazzip.length !== 0 ? (
+        <div className="jangSoModal__pickedJangso__container">
+          {reduxState.arrForPickMazzip.map((local, index) => {
+            return <PickedMazzip local={local} key={index} />;
+          })}
+        </div>
+      ) : (
+        <div className="jangSoModal__desc">
+          <span>가고 싶은 맛집들을 검색하여 추가해주세요</span>
+          <FontAwesomeIcon icon={faPlus} className="jangSoModal__plus-btn" />
+        </div>
+      )}
+    </div>
+  );
+}
+
 function PickedLocation({ local }) {
   let [timeVal, setTimeVal] = useState(2);
   let [minVal, setMinVal] = useState(0);
@@ -298,32 +369,6 @@ function PickedLocation({ local }) {
             maxLength={5}
             defaultValue={timeVal}
             style={{ width: "40px", fontSize: "20px", color: "var(--orange)" }}
-            // onClick={(e) => {
-            //   if (e.target.value !== 0) {
-            //     if (timeVal > e.target.value) dispatch(timeSetter(-1));
-            //     else dispatch(timeSetter(1));
-            //     setTimeVal(e.target.value);
-            //     dispatch(addAtimeArrForJangso(timeVal * 60 * 60 * 1000));
-            //   }
-            // }}
-            // onKeyUp={(e) => {
-            //   if (e.code === "Enter") {
-            //     if (timeVal > e.target.value)
-            //       dispatch(timeSetter(e.target.value - timeVal));
-            //     else dispatch(timeSetter(e.target.value - timeVal));
-            //     setTimeVal(e.target.value);
-            //     dispatch(addAtimeArrForJangso(timeVal * 60 * 60 * 1000));
-            //   }
-            // }}
-            // onBlur={(e) => {
-            //   if (e.target.value !== 0) {
-            //     if (timeVal > e.target.value)
-            //       dispatch(timeSetter(e.target.value - timeVal));
-            //     else dispatch(timeSetter(e.target.value - timeVal));
-            //     setTimeVal(e.target.value);
-            //     dispatch(addAtimeArrForJangso(timeVal * 60 * 60 * 1000));
-            //   }
-            // }}
             onChange={(e) => {
               e.stopPropagation();
               if (
@@ -349,32 +394,111 @@ function PickedLocation({ local }) {
             max={59}
             style={{ width: "40px", fontSize: "20px", color: "var(--orange)" }}
             defaultValue={minVal}
-            // onClick={(e) => {
-            //   if (e.target.value !== 0) {
-            //     if (minVal > e.target.value) dispatch(minSetter(-1));
-            //     else if (minVal < e.target.value) dispatch(minSetter(1));
-            //     setMinVal(e.target.value);
-            //     dispatch(addAtimeArrForJangso(minVal * 60 * 1000));
-            //   }
-            // }}
-            // onKeyUp={(e) => {
-            //   if (e.code === "Enter") {
-            //     if (minVal > e.target.value)
-            //       dispatch(minSetter(e.target.value - minVal));
-            //     else dispatch(minSetter(e.target.value - minVal));
-            //     setMinVal(e.target.value);
-            //     dispatch(addAtimeArrForJangso(minVal * 60 * 1000));
-            //   }
-            // }}
-            // onBlur={(e) => {
-            //   if (e.target.value !== 0) {
-            //     if (minVal > e.target.value)
-            //       dispatch(minSetter(e.target.value - minVal));
-            //     else dispatch(minSetter(e.target.value - minVal));
-            //     setMinVal(e.target.value);
-            //     dispatch(addAtimeArrForJangso(minVal * 60 * 1000));
-            //   }
-            // }}
+            onChange={(e) => {
+              e.stopPropagation();
+              if (
+                reduxState.leftSideTimeSetter * 60 + reduxState.leftSideMinSetter >
+                reduxState.tripPeriod.length * 1440
+              ) {
+                alert("여행일자 x 24시간을 초과할 수 없습니다");
+                setMinVal(minVal);
+                // return;
+              }
+              // if (e.target.value !== 0) {
+              if (e.target.value >= 60) {
+                timeVal++;
+                e.target.value = 0;
+              }
+              if (minVal > e.target.value) dispatch(minSetter(e.target.value - minVal));
+              else dispatch(minSetter(e.target.value - minVal));
+              setMinVal(e.target.value);
+              dispatch(addAtimeArrForJangso(minVal * 60 * 1000));
+              // }
+            }}
+          />
+          <span style={{ fontSize: "13px" }}>분</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function PickedMazzip({ local }) {
+  let [timeVal, setTimeVal] = useState(2);
+  let [minVal, setMinVal] = useState(0);
+  let dispatch = useDispatch();
+  let reduxState = useSelector((state) => {
+    return state;
+  });
+  return (
+    <div className="pickedLocation__container slide-in-right">
+      <div
+        className="pickedLocation__img-container"
+        onClick={(e) => {
+          e.stopPropagation();
+          dispatch(changeLnfM(local));
+        }}
+      >
+        <img
+          src={local.smallimage ?? "/images/no-image.jpg"}
+          onError={(e) => {
+            e.target.src = "/images/no-image.jpg";
+          }}
+        />
+      </div>
+      <div className="pickedLocation__info-container">
+        <div className="pickedLocation__info-title">
+          <div>
+            <h5>{local.title}</h5>
+          </div>
+          <FontAwesomeIcon
+            icon={faX}
+            className="pickedLocation__info-title__closeBtn"
+            onClick={(e) => {
+              e.stopPropagation();
+              dispatch(delPickMazzip(local));
+              console.log("맛집", local);
+              dispatch(timeSetter(-2));
+              dispatch(minSetter(-0));
+              dispatch(addArrInForRestaurant(local));
+            }}
+          />
+        </div>
+        <div className="pickedLocation__time-container">
+          <FontAwesomeIcon icon={faClock} style={{ marginRight: "3px" }} />
+          <input
+            type="number"
+            name={1}
+            min={0}
+            max={24}
+            maxLength={5}
+            defaultValue={timeVal}
+            style={{ width: "40px", fontSize: "20px", color: "var(--orange)" }}
+            onChange={(e) => {
+              e.stopPropagation();
+              if (
+                reduxState.leftSideTimeSetter * 60 + reduxState.leftSideMinSetter >
+                reduxState.tripPeriod.length * 1440
+              ) {
+                alert("여행일자 x 24시간을 초과할 수 없습니다");
+                setTimeVal(timeVal);
+                // return;
+              }
+              // if (e.target.value !== 0) {
+              if (timeVal > e.target.value) dispatch(timeSetter(e.target.value - timeVal));
+              else dispatch(timeSetter(e.target.value - timeVal));
+              setTimeVal(e.target.value);
+              dispatch(addAtimeArrForJangso(timeVal * 60 * 60 * 1000));
+              // }
+            }}
+          />
+          <span style={{ fontSize: "13px" }}>시간</span>
+          <input
+            type="number"
+            min={0}
+            max={59}
+            style={{ width: "40px", fontSize: "20px", color: "var(--orange)" }}
+            defaultValue={minVal}
             onChange={(e) => {
               e.stopPropagation();
               if (
