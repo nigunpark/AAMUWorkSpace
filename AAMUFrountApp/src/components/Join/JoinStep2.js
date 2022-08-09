06336004afe50782a8fcdd1fce2660ab90594428
jiyellie <106718961+jiyellie@users.noreply.github.com>
2022-08-09 18:38:43 +0900

    Update JoinStep2.js

--- AAMUFrountApp/src/components/Join/JoinStep2.js
+++ AAMUFrountApp/src/components/Join/JoinStep2.js
@@ -642,7 +642,7 @@ const SendEmail = ({
           <span
             className="emailValid-confirm-btn"
             onClick={() => {
-              if (userVNumRef.current.value === number) {
+              if (parseInt(userVNumRef.current.value) === number) {
                 setEmailCk(true);
               }
             }}
