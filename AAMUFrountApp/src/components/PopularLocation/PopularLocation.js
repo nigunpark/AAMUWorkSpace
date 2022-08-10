8bf320cb39eea251993a0f270acf08e6b7fc242a
MoZZANG <kkm0938@naver.com>
2022-08-10 19:09:59 +0900

    Update PopularLocation.js

--- AAMUFrountApp/src/components/PopularLocation/PopularLocation.js
+++ AAMUFrountApp/src/components/PopularLocation/PopularLocation.js
@@ -3,26 +3,27 @@ import { Link, useNavigate } from "react-router-dom";
 import "./PopularLocation.css";
 import { changeLnfM } from "../../redux/store";
 import { useDispatch } from "react-redux";
-const PopularLocation = () => {
+const PopularLocation = ({ val, i }) => {
   let localRef = useRef("");
   let navigate = useNavigate();
   let dispatch = useDispatch();
   return (
     <li
       className="PopularLocation__li"
-      onClick={() => {
-        dispatch(changeLnfM(localRef.current.textContent));
-        navigate("/mainPage/" + localRef.current.textContent);
-      }}>
+      // onClick={() => {
+      //   dispatch(changeLnfM(localRef.current.textContent));
+      //   navigate("/mainPage/" + localRef.current.textContent);
+      // }}
+    >
       <div>
-        <span className="PopularLocation__rank">1</span>
+        <span className="PopularLocation__rank">{i + 1}</span>
         <span className="title" ref={localRef}>
-          춘천
+          {val.title}
         </span>
       </div>
       <div>
         <i className="fa-solid fa-sort-up" />
-        <span className="count">2</span>
+        <span className="count">{val.count}</span>
       </div>
     </li>
   );
