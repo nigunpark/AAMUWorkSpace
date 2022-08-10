a9aaf1f73b1f271317250b7269197cf606d3e046
MoZZANG <kkm0938@naver.com>
2022-08-10 19:10:07 +0900

    Update WholeMap.js

--- AAMUFrountApp/src/pages/WholeMap.js
+++ AAMUFrountApp/src/pages/WholeMap.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../components/WholeMap/WholeMap.css";
 import WholeMapCoords from "../components/WholeMap/WholeMapCoords.js";
 import WholeMapLocalData from "../components/WholeMap/WholeMapLocalData.js";
@@ -7,6 +7,7 @@ import PopularLocation from "../components/PopularLocation/PopularLocation";
 import { Link, useNavigate } from "react-router-dom";
 import { changeLnfM } from "../redux/store";
 import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
 
 const WholeMap = () => {
   const [modalState, setModalState] = useState(false);
@@ -16,7 +17,8 @@ const WholeMap = () => {
     return state;
   });
   let dispatch = useDispatch();
-
+  console.log("localId", localId);
+  console.log("localName", localName);
   return (
     <div className="WholeMap">
       {/* <video src="/images/video/video.mp4" muted loop autoPlay /> */}
@@ -64,6 +66,83 @@ const WholeMap = () => {
 
 //모달창
 function Modal({ setModalState, localName, localId }) {
+  const [recomLocals, setRecomLocals] = useState([]);
+  async function getRecommendLocal(localName) {
+    let token = sessionStorage.getItem("token");
+    let areacode;
+    switch (localName) {
+      case "서울":
+        areacode = 1;
+        break;
+      case "인천":
+        areacode = 2;
+        break;
+      case "대전":
+        areacode = 3;
+        break;
+      case "대구":
+        areacode = 4;
+        break;
+      case "광주광역시":
+        areacode = 5;
+        break;
+      case "부산":
+        areacode = 6;
+        break;
+      case "울산":
+        areacode = 7;
+        break;
+      case "세종":
+        areacode = 8;
+        break;
+      case "경기도":
+        areacode = 31;
+        break;
+      case "강원도":
+        areacode = 32;
+        break;
+      case "충청북도":
+        areacode = 33;
+        break;
+      case "충청남도":
+        areacode = 34;
+        break;
+      case "경상북도":
+        areacode = 35;
+        break;
+      case "경상남도":
+        areacode = 36;
+        break;
+      case "전라북도":
+        areacode = 37;
+        break;
+      case "전라남도":
+        areacode = 38;
+        break;
+      case "제주도":
+        areacode = 39;
+        break;
+    }
+    await axios
+      .get("/aamurest/main/area", {
+        params: {
+          areacode: areacode,
+        },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((resp) => {
+        console.log(resp);
+        setRecomLocals(resp.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  useEffect(() => {
+    getRecommendLocal(localName);
+  }, []);
   return (
     <Container>
       <Overlay
@@ -83,10 +162,12 @@ function Modal({ setModalState, localName, localId }) {
             </Link>
           </div>
           <ul className="local__modal__ul">
+            {recomLocals.map((val, i) => {
+              return <PopularLocation val={val} key={i} i={i} />;
+            })}
+            {/* <PopularLocation />
             <PopularLocation />
-            <PopularLocation />
-            <PopularLocation />
-            <PopularLocation />
+            <PopularLocation /> */}
           </ul>
         </Body>
       </Contents>
