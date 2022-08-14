32d542f3964e17e646459551b011b52e918b5d29
MoZZANG <kkm0938@naver.com>
2022-08-14 15:05:50 +0900

    Update Navbar.js

--- AAMUFrountApp/src/components/Navbar/Navbar.js
+++ AAMUFrountApp/src/components/Navbar/Navbar.js
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMessage, faRobot } from "@fortawesome/free-solid-svg-icons";
 import ChatBot from "../ChatBot/ChatBot";
 import axios from "axios";
+import { useSelector } from "react-redux";
 let chatArr = [];
 const Navbar = ({ scrollNav, whereUrl, noti, setNoti }) => {
   const [click, setClick] = useState(false);
@@ -17,11 +18,24 @@ const Navbar = ({ scrollNav, whereUrl, noti, setNoti }) => {
     color: "var(--orange)",
     fontWeight: "bold",
   };
+  const [showChatPrev, setShowChatPrev] = useState(false);
   let navigate = useNavigate();
   let location = useLocation();
+  let reduxState = useSelector((state) => state);
+  let chatPrevBoxRef = useRef();
   useEffect(() => {
     if (showChatBot === false) chatArr = [];
   }, [showChatBot]);
+  useEffect(() => {
+    setShowChatPrev(true);
+    setTimeout(() => {
+      chatPrevBoxRef.current.classList.add("chatPreviewBox__out");
+    }, 3500);
+    setTimeout(() => {
+      setShowChatPrev(false);
+    }, 4000);
+  }, [reduxState.chatPreview]);
+  // console.log("chatPreview", reduxState.chatPreview[0].authid);
   return (
     <div className="navbar__fragment">
       <nav
@@ -117,6 +131,34 @@ const Navbar = ({ scrollNav, whereUrl, noti, setNoti }) => {
       <Outlet />
       {location.pathname.indexOf("mainPage") !== 1 && location.pathname.indexOf("forum") !== 1 && (
         <ChatBotBtn setShowChatBot={setShowChatBot} showChatBot={showChatBot} />
+      )}
+      {showChatPrev && (
+        <div className="chatPreviewBox" ref={chatPrevBoxRef}>
+          <div className="chatPreviewBox__container">
+            <div className="chatPreviewBox__top">
+              <span style={{ fontSize: "19px", fontWeight: "bold" }}>
+                {reduxState.chatPreview[0].authid}
+              </span>
+              <span style={{ fontSize: "14px" }}>
+                {(new window.Date(reduxState.chatPreview[0].senddate).getHours() >= 12 &&
+                new window.Date(reduxState.chatPreview[0].senddate).getMinutes() >= 1
+                  ? "오후"
+                  : "오전") +
+                  (new window.Date(reduxState.chatPreview[0].senddate).getHours() > 12
+                    ? new window.Date(reduxState.chatPreview[0].senddate).getHours() - 12
+                    : new window.Date(reduxState.chatPreview[0].senddate).getHours()) +
+                  ":" +
+                  new window.Date(reduxState.chatPreview[0].senddate)
+                    .getMinutes()
+                    .toString()
+                    .padStart(2, "0")}
+              </span>
+            </div>
+            <div className="chatPreviewBox__bottom">
+              <p>{reduxState.chatPreview[0].missage}</p>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
