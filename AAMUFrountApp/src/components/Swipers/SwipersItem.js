86e4481e7ac1284206ccd7e406ac6e295cab8398
MoZZANG <kkm0938@naver.com>
2022-08-17 17:05:47 +0900

    Update SwipersItem.js

--- AAMUFrountApp/src/components/Swipers/SwipersItem.js
+++ AAMUFrountApp/src/components/Swipers/SwipersItem.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const SwipersItem = (props) => {
+  console.log("props", props);
   return (
     <>
       <li className="swiper__item">
         <Link to="/" className="swiper__item__link">
           <figure className="swiper__item__pic-wrap" data-category={props.label}>
-            <img src={props.src} alt="travel image" className="swiper__item__img" />
+            <img
+              src={props.src ?? `images/no-image.jpg`}
+              alt="travel image"
+              className="swiper__item__img"
+            />
           </figure>
           <div className="swiper__item__info">
             <h5 className="swiper__item__text">{props.text}</h5>
