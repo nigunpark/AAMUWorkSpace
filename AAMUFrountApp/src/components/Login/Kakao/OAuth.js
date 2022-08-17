943f3ceb30d479e555d02ce2a29a77f5d6bb6b0f
MoZZANG <kkm0938@naver.com>
2022-08-17 17:05:43 +0900

    Update OAuth.js

--- AAMUFrountApp/src/components/Login/Kakao/OAuth.js
+++ AAMUFrountApp/src/components/Login/Kakao/OAuth.js
@@ -1,4 +1,3 @@
 const CLIENT_ID = "3a141ff9d81872fc2f019e5f2f7de619";
-const REDIRECT_URI =  "http://localhost:3000/oauth/callback/kakao";
+const REDIRECT_URI = "http://192.168.0.35:3000/oauth/callback/kakao";
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-    
